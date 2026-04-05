cb0dc24f4d40db6b52c431591e9d878686622df2
alina1701512-boop <alina1701512@gmail.com>
2026-04-05 18:18:43 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import DeclarativeBase
 from config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+# Настраиваем движок базы данных
+# connect_args={"ssl": "require"} нужно для Neon.tech
+engine = create_async_engine(DATABASE_URL, echo=False, connect_args={"ssl": "require"})
+
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(DeclarativeBase):
