8fb83d24c9fcbfa43ca3bf3e7a212aa3cff05506
alina1701512-boop <alina1701512@gmail.com>
2026-04-07 10:48:41 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -10,6 +11,10 @@ class Task(Base):
     title = Column(String, nullable=False)
     due_at = Column(DateTime, nullable=True)
     is_done = Column(Boolean, default=False)
-    priority = Column(String, default="yellow")
+    priority = Column(String, default="none")
     repeat_rule = Column(String, default="none")
     is_reminded = Column(Boolean, default=False)
+    
+    # Новые поля
+    created_at = Column(DateTime, default=datetime.utcnow)
+    is_archived = Column(Boolean, default=False)
