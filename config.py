62922230e147118f25b5389b1a7bd7d3aa0c592e
alina1701512-boop <alina1701512@gmail.com>
2026-04-08 11:21:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ TG_TOKEN = os.getenv("TG_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 APP_HOST = os.getenv("RENDER_EXTERNAL_URL", "http://localhost:8000").rstrip("/")
 TZ = os.getenv("TZ", "Europe/Moscow")
+
+# 🔥 Google Calendar
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
