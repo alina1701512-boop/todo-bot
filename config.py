67d0f15a53ab43a6a15aade82cbc77d65cd16b8c
alina1701512-boop <alina1701512@gmail.com>
2026-04-11 21:29:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TG_TOKEN = os.getenv("TG_BOT_TOKEN")
+TG_TOKEN = os.getenv("TG_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 APP_HOST = os.getenv("RENDER_EXTERNAL_URL", "http://localhost:8000").rstrip("/")
 TZ = os.getenv("TZ", "Europe/Moscow")
